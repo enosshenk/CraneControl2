afd7940341cdc4891b4f0018b659fc0078bd45f3
Enos Shenk <enosshenk@gmail.com>
2022-09-20 13:04:00 -0400

    Update CraneControl2.ino

--- CraneControl2.ino
+++ CraneControl2.ino
@@ -1,9 +1,10 @@
 #include <IBusBM.h>
 
 IBusBM IBus;        // IBus object
-int Channel[5];     // Array for raw channel data
-float ChannelF[5];  // Cleaned up array
-bool Direction[5];  // Direction array
+int Channel[6] = {0, 0, 0, 0, 0, 0};     // Array for raw channel data
+int ChannelLast[6] = {0, 0, 0, 0, 0, 0}; // Previous loop channel data
+float ChannelF[6];  // Cleaned up array
+bool Direction[6];  // Direction array
 
 // PINS
 int M1IN1 = 22; int M1IN2 = 24; int M1IN3 = 26; int M1IN4 = 28; 
@@ -13,8 +14,12 @@ int M4IN1 = 46; int M4IN2 = 48; int M4IN3 = 50; int M4IN4 = 52;
 
 int M1ENA = 2;  int M2ENA = 4;  int M3ENA = 6;  int M4ENA = 8;  
 int M1ENB = 3;  int M2ENB = 5;  int M3ENB = 7;  int M4ENB = 9;
-
+int Green = 11;
+int Red = 12;
 int RLY1 = 13;
+
+bool LinkUp = false;    // True if transmitter is talking to us
+int SameReads = 0;      // Counts identical channel reads for determining connection
 
 void setup() {
   Serial.begin(115200);
@@ -34,59 +39,143 @@ void setup() {
 
   // Relay
   pinMode(13, OUTPUT);
+
+  // LED
+  pinMode(Green, OUTPUT);
+  pinMode(Red, OUTPUT);
 }
 
 void loop() {
-  
+  // Initial linkup
+  if (IBus.cnt_rec > 0) { LinkUp = true; }
+
+  // Transfer current channel data to last array
+  for (int i=0; i<6; i++) {
+    ChannelLast[i] = Channel[i];     
+  }
+
+  // Read current channel data
   for (int i=0; i<6; i++) {
     Channel[i] = IBus.readChannel(i);
-    Serial.println(Channel[i]);
-    if (Channel[i] < 1500) { Direction[i] = false; } else if (Channel[i] > 1500) { Direction[i] = true; }
+    float temp = ((float)Channel[i] - 2000.0) / 500.0;
+    ChannelF[i] = temp + 1.0;
   }
-  
-  UpdateModule1();
-//  printChannels();
+
+  // Compare current data to last data
+  if (Channel[0] == ChannelLast[0] && 
+  Channel[1] == ChannelLast[1] && 
+  Channel[2] == ChannelLast[2] && 
+  Channel[3] == ChannelLast[3]) {
+    // Same data, increment counter
+    SameReads++;
+  } else {
+    // Data changed, we're still linked
+    LinkUp = true;
+    SameReads = 0;
+  }
+  if (SameReads > 1000) {
+    // If we've got a ton of same reads, flag link is down
+    LinkUp = false;
+  }
+
+  // Control the LED for link status
+  if (LinkUp) {
+    digitalWrite(Green, HIGH);
+    digitalWrite(Red, LOW);
+  }
+  else
+  {
+    digitalWrite(Green, LOW);
+    digitalWrite(Red, HIGH);
+  }
+
+  // Update modules
+  //  UpdateModule1();
+  UpdateModule2();
+  //  printChannels();
 
   delay(100);
 }
 
-void UpdateModule1() {
- // Serial.println(ChannelF[1]);
-  if (Direction[1] == true) {
- //   Serial.println("Forward");
+void UpdateModule1() {  
+  if (ChannelF[2] > 0.05) {
     // Motor 1 forward
     digitalWrite(M1IN1, HIGH );
     digitalWrite(M1IN2, LOW);
-    int temp = map(Channel[1], 1000, 1500, 0, 255);
-    analogWrite(M1ENA, temp);
+    float temp = ChannelF[2] * 255;
+    analogWrite(M1ENA, (int)temp);
   }
-  else if (Direction[1] == false) {
-  //  Serial.println("Reverse");
+  else if (ChannelF[2] < -0.05) {
     // Motor 1 reverse
     digitalWrite(M1IN1, LOW);
     digitalWrite(M1IN2, HIGH);
-    int temp = map(Channel[1], 1500, 2000, 0, 255);
-    analogWrite(M1ENA, temp);
-  } 
-  
-  if (Direction[2] == true) {
-//    Serial.println("Forward");
-    // Motor 2 forward
-    digitalWrite(M1IN3, HIGH );
-    digitalWrite(M1IN4, LOW);
-    int temp = map(Channel[2], 1000, 1500, 0, 255);
-    analogWrite(M1ENB, temp);
+    float temp = ChannelF[2] * -1;
+    analogWrite(M1ENA, temp * 255);
   }
-  else if (Direction[2] == false) {
-//    Serial.println("Reverse");
-    // Motor 2 reverse
+  else
+  {
+    analogWrite(M1ENA, 0);
+  }
+  
+  if (ChannelF[1] > 0.05) {
+    // Motor 2 forward
     digitalWrite(M1IN3, LOW);
     digitalWrite(M1IN4, HIGH);
-    int temp = map(Channel[2], 1500, 2000, 0, 255);
-    analogWrite(M1ENB, temp);
-  } 
+    float temp = ChannelF[1] * 255;
+    analogWrite(M1ENB, (int)temp);
+  }
+  else if (ChannelF[1] < -0.05) {
+    // Motor 2 reverse
+    digitalWrite(M1IN3, HIGH);
+    digitalWrite(M1IN4, LOW);
+    float temp = ChannelF[1] * -1;
+    analogWrite(M1ENB, temp * 255);
+  }
+  else
+  {
+    analogWrite(M1ENB, 0);
+  }
 }
 
+void UpdateModule2() {  
+  if (ChannelF[2] > 0.05) {
+    // Motor 3 forward
+    digitalWrite(M2IN1, HIGH );
+    digitalWrite(M2IN2, LOW);
+    float temp = ChannelF[2] * 255;
+    analogWrite(M2ENA, (int)temp);
+  }
+  else if (ChannelF[2] < -0.05) {
+    // Motor 3 reverse
+    digitalWrite(M2IN1, LOW);
+    digitalWrite(M2IN2, HIGH);
+    float temp = ChannelF[2] * -1;
+    analogWrite(M2ENA, temp * 255);
+  }
+  else
+  {
+    analogWrite(M2ENA, 0);
+  }
+  
+  if (ChannelF[1] > 0.05) {
+    // Motor 4 forward
+    digitalWrite(M2IN3, LOW);
+    digitalWrite(M2IN4, HIGH);
+    float temp = ChannelF[1] * 255;
+    analogWrite(M2ENB, (int)temp);
+  }
+  else if (ChannelF[1] < -0.05) {
+    // Motor 4 reverse
+    digitalWrite(M2IN3, HIGH);
+    digitalWrite(M2IN4, LOW);
+    float temp = ChannelF[1] * -1;
+    analogWrite(M2ENB, temp * 255);
+  }
+  else
+  {
+    analogWrite(M2ENB, 0);
+  }
+}
 void printChannels() {
   for (int i=0; i<6; i++) {
  //   Serial.print("Channel ");
